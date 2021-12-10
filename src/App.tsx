aad38f8f184f522c3364aefbfb618526228f184d
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-12-10 08:27:10 +0200

    construct the high level components flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import LandingPage from "./components/LandingPage";
+import QuestionPage from "./components/QuestionPage";
+import ThanksPage from "./components/ThanksPage";
+import { AppState } from "./types";
+
 function App() {
-  return <div></div>;
+  const [currentState, setCurrentState] = useState<AppState>("landing");
+
+  const states = {
+    landing: <LandingPage />,
+    questions: <QuestionPage />,
+    thanks: <ThanksPage />,
+  };
+
+  return <div>{states[currentState]}</div>;
 }
 
 export default App;
