2315bcb7e5267027727fc32b710349f6a89a982b
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-12-10 10:39:46 +0200

    put every thing together

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import LandingPage from "./components/LandingPage";
 import QuestionPage from "./components/QuestionPage";
 import ThanksPage from "./components/ThanksPage";
 import { AppState } from "./types";
+import styles from "./assets/styles/app.module.css";
 
 function App() {
   const [currentState, setCurrentState] = useState<AppState>("landing");
 
+  const goNext = (newState: AppState) => setCurrentState(newState);
+
   const states = {
-    landing: <LandingPage />,
+    landing: <LandingPage goNext={goNext} />,
     questions: <QuestionPage />,
     thanks: <ThanksPage />,
   };
 
-  return <div>{states[currentState]}</div>;
+  return <div className={styles.app}>{states[currentState]}</div>;
 }
 
 export default App;
