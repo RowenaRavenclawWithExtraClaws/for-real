5efa92bbd7855324bc4f16d32a4aba72c652cc49
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-12-10 10:38:56 +0200

    add types and interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,30 @@
+export interface TextProps {
+  variant:
+    | "button"
+    | "caption"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "subtitle1"
+    | "subtitle2"
+    | "body1"
+    | "body2";
+
+  text: string;
+}
+
+export interface ButtonProps {
+  variant: "text" | "outlined" | "contained";
+  text: string;
+  style: object;
+  clickHandler: any;
+}
+
+export interface LangingPageProps {
+  goNext: (param: AppState) => void;
+}
+
 export type AppState = "landing" | "questions" | "thanks";
