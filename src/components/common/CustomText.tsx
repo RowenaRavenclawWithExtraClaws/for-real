4df1820df98b6dcab821439eff375ba97ab821fd
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-12-10 10:39:29 +0200

    implement custom text component

--- src/components/common/CustomText.tsx
+++ src/components/common/CustomText.tsx
@@ -1,5 +1,13 @@
-const CustomText = () => {
-  return <div></div>;
+import { Typography } from "@mui/material";
+import { TextProps } from "../../types";
+import styles from "../../assets/styles/common/text.module.css";
+
+const CustomText = (props: TextProps) => {
+  return (
+    <Typography className={styles.customText} variant={props.variant}>
+      {props.text}
+    </Typography>
+  );
 };
 
 export default CustomText;
