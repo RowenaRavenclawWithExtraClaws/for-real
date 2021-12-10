053b616dc64e0af5de16481f9d4ea41368aae41b
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-12-10 10:39:16 +0200

    implement custom button component

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,5 +1,16 @@
-const CustomButton = () => {
-  return <div></div>;
+import { Button } from "@mui/material";
+import { ButtonProps } from "../../types";
+
+const CustomButton = (props: ButtonProps) => {
+  return (
+    <Button
+      sx={{ ...props.style }}
+      variant={props.variant}
+      onClick={props.clickHandler}
+    >
+      {props.text}
+    </Button>
+  );
 };
 
 export default CustomButton;
